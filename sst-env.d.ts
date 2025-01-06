2ea853acd4bdd7e9c382a61d4aa54cda73fa971e
Chase Johnson <cjohnson19@pm.me>
2025-01-06 15:58:01 -0600

    Fix ETAPS based conference dates

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -28,6 +28,25 @@ declare module "sst" {
           "type": "conference"
           "url": string
         }
+        "ESOP": {
+          "abbreviation": string
+          "date": {
+            "end": string
+            "start": string
+          }
+          "importantDateUrl": string
+          "importantDates": {
+            "camera-ready": string
+            "notification": string
+            "paper": string
+            "rebuttal": string
+          }
+          "location": string
+          "name": string
+          "tags": any
+          "type": "conference"
+          "url": string
+        }
         "FoSSaCS": {
           "abbreviation": string
           "date": {
@@ -37,6 +56,7 @@ declare module "sst" {
           "importantDateUrl": string
           "importantDates": {
             "abstract": string
+            "camera-ready": string
             "notification": string
             "paper": string
           }
@@ -44,7 +64,6 @@ declare module "sst" {
           "name": string
           "tags": any
           "type": "conference"
-          "url": string
         }
         "ICFP": {
           "abbreviation": string
